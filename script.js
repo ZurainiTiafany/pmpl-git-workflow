75d51d023eaf054902c36595a772bea2d9526461
Nuraliza <nuralizashafira29@gmail.com>
2026-03-10 21:30:34 +0700

    feat: mengubah teks halaman

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 // Git Workflow Simulation Script
 
 function showMessage() {
-    alert("Halo! Selamat Datang di Halaman ini.");
-
-    let paragrafBaru = document.createElement("p");
-    paragrafBaru.innerText = "Ini adalah paragraf baru";
+    document.querySelector("p").innerText =
+    "Teks ini diubah untuk menguji perubahan kedua.";
 }
 
 console.log("Git Workflow Simulation loaded.");
