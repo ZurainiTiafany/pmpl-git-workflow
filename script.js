2d135b8cd326f888020233228ce4a46bd3cedc34
Nuraliza <nuralizashafira29@gmail.com>
2026-03-10 21:18:50 +0700

    feat: melakukan perubahan teks

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 
 function showMessage() {
     alert("Halo! Selamat Datang di Halaman ini.");
+
+    document.querySelector("p").innerText =
+    "Teks ini diubah untuk menguji perubahan kedua.";
 }
 
 console.log("Git Workflow Simulation loaded.");
