0fb28968c431eb9b6deabbe62580ed0dce0daf9a
Nuraliza <nuralizashafira29@gmail.com>
2026-03-10 21:35:49 +0700

    feat: menambahkan paragraf baru

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Git Workflow Simulation Script
 
 function showMessage() {
-    document.querySelector("p").innerText =
-    "Teks ini diubah untuk menguji perubahan kedua.";
+    let paragrafBaru = document.createElement("p");
+    paragrafBaru.innerText = "Ini adalah paragraf baru hasil perubahan ketiga pada script.js.";
+
+    document.body.appendChild(paragrafBaru);
 }
 
 console.log("Git Workflow Simulation loaded.");
